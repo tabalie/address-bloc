e0c1d0d1b00a7598c5918dd693b89fe42c42466b
Natalie Kueneman <tabalie@gmail.com>
2015-06-08 14:28:48 -0600

    First commit - CORRECTED - for Assignment 23, created a new test in spec/address_bloc_spec.rb that will use data from a second csv file entries_2.csv

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -88,24 +88,6 @@ RSpec.describe AddressBook do
         expect(book_size).to eql 3
       end
 
-      it "imports the 1st entry" do
-        book.import_from_csv("entries_2.csv")
-        entry_one = book.entries[0]
-        check_entry(entry_one, "Bill TWo", "555-555-5555", "bill@blocmail.com")
-      end
-
-      it "imports the 2nd entry" do
-        book.import_from_csv("entries_2.csv")
-        entry_two = book.entries[1]
-        check_entry(entry_two, "Bob Two", "555-555-5555", "bob@blocmail.com")
-      end
- 
-      it "imports the 3rd entry" do
-        book.import_from_csv("entries_2.csv")
-        entry_three = book.entries[2]
-        check_entry(entry_three, "Joe Two", "555-555-5555", "joe@blocmail.com")
-      end
-
    end
  
 end
